e286a9f864c7351c3b5299bf732a99833cbd59d9
B66780 <andrewrodriguez@fullsail.edu>
2014-04-10 20:53:31 -0400

    added first files to the repo.

---
+++ Rodriguez_Andrew_Assignment2.js
@@ -0,0 +1,9 @@
+// Andrew Rodriguez	
+// SDI - 1404
+// Assignment 2 Project 2
+// JavaScript - FlowChart
+// "Dragon" "Slaying" "Armor" "Weapon
+
+// Preparing to fight a dragon! Deciding which armor to get and what weapon to use.
+
+// 
